801915f0957f77eb8ceda9b44acc4bf8123fc94e
John <john.bounds@student.kuleuven.be>
2025-11-25 03:24:07 +0100

    Build ready for tomorrow

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(path.join(__dirname, 'resources'))); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'pug');
+
+const port = 8000;
+
+app.get('/', (req, res) => {
+    res.status(200).render("home");
+});
+
+app.get('/11252025', (req, res) => {
+    res.status(200).render('11252025');
+});
+
+app.get('/11252025/respectme', (req, res) => {
+    res.sendFile(path.join(__dirname, 'resources/html/respectme.html'));
+})
+
+
+app.use((req, res) => {
+    res.status(404).render('404');
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
